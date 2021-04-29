a7ff32671550bab7eddf7c7b93e12991cfe2fdd8
Raubezh <raubezhdmitriy@gmail.com>
2021-04-29 12:47:09 +0300

    fix homework 1

--- src/main/java/model/Kotik.java
+++ src/main/java/model/Kotik.java
@@ -1,36 +1,34 @@
 package model;
 
 public class Kotik {
+    private int satiety;
+    private String name;
+    private String meow;
+    private int weight;
+    private int prettiness;
+
     private static int catCount = 0;
 
     public static int getCatCount() {
         return catCount;
     }
 
-    public static int count = 1;
-
-    private int full;
-    private String name;
-    private String meow;
-    private int weight;
-    private int prettiness;
-
     public Kotik() {
         catCount++;
     }
 
-    public Kotik(int prettiness, String name, String meow, int weight, int full) {
+    public Kotik(int prettiness, String name, String meow, int weight, int satiety) {
         this.prettiness += prettiness;
         this.name = name;
         this.meow = meow;
         this.weight = weight;
-        this.full = full;
+        this.satiety = satiety;
         catCount++;
     }
 
     public void setKotik(int prettiness, String name, int weight, String meow) {
         this.prettiness += prettiness;
-        full = 5;
+        satiety = 5;
         this.name = name;
         this.meow = meow;
         this.weight = weight;
@@ -39,24 +37,28 @@ public class Kotik {
     public int getPrettiness() {
         return prettiness;
     }
-    public int getFull() {
-        return full;
+
+    public int getSatiety() {
+        return satiety;
     }
+
     public String getName() {
         return name;
     }
+
     public String getMeow() {
         return meow;
     }
+
     public int getWeight() {
         return weight;
     }
 
     public void liveAnotherDay() {
-        for (int i = 0; i < 24; i++) {
+        for (int i = 1; i < 25; i++) {
             int random = 1 + (int) (Math.random() * 5);
-            System.out.println(count++);
-            if (full <= 0) {
+            System.out.println(i);
+            if (getSatiety() <= 0) {
                 System.out.println("Cat is hungry");
                 switch (random) {
                     case 1:
@@ -74,33 +76,34 @@ public class Kotik {
                     default:
                         eat();
                 }
-            }
-            switch (random) {
-                case 1:
-                    play();
-                    break;
-                case 2:
-                    eat();
-                    break;
-                case 3:
-                    sleep();
-                    break;
-                case 4:
-                    chaseMouse();
-                    break;
-                default:
-                    washing();
+            } else {
+                switch (random) {
+                    case 1:
+                        play();
+                        break;
+                    case 2:
+                        eat();
+                        break;
+                    case 3:
+                        sleep();
+                        break;
+                    case 4:
+                        chaseMouse();
+                        break;
+                    default:
+                        washing();
+                }
             }
         }
     }
 
     private void eat(int nutritionFacts) {
-        full += nutritionFacts;
-        System.out.println("Cat eating " + "\nFullness: " + full + "\n");
+        satiety += nutritionFacts;
+        System.out.println("Cat eating " + "\nFullness: " + satiety + "\n");
     }
 
     private void eat(int nutritionFacts, String food) {
-        full += nutritionFacts;
+        satiety += nutritionFacts;
         System.out.println("Cat eating " + food + "\nFullness: " + nutritionFacts + "\n");
     }
 
@@ -109,46 +112,43 @@ public class Kotik {
     }
 
     private boolean play() {
-        if (full < 0) {
+        if (satiety < 0) {
             return false;
-        } else {
-            full -= 3;
-            System.out.println("Cat playing");
-            System.out.println("Fullness: " + full + "\n");
-            return true;
         }
+        satiety -= 3;
+        System.out.println("Cat playing");
+        System.out.println("Fullness: " + satiety + "\n");
+        return true;
     }
 
     private boolean sleep() {
-        if (full < 0) {
+        if (satiety <= 0) {
             return false;
-        } else {
-            full -= 2;
-            System.out.println("Cat sleeping");
-            System.out.println("Fullness: " + full + "\n");
-            return true;
         }
+        satiety -= 2;
+        System.out.println("Cat sleeping");
+        System.out.println("Fullness: " + satiety + "\n");
+        return true;
     }
 
     private boolean chaseMouse() {
-        if (full < 0) {
+        if (satiety <= 0) {
             return false;
-        } else {
-            full -= 3;
-            System.out.println("Cat chasing mouse");
-            System.out.println("Fullness: " + full + "\n");
-            return true;
         }
+        satiety -= 3;
+        System.out.println("Cat chasing mouse");
+        System.out.println("Fullness: " + satiety + "\n");
+        return true;
     }
 
     private boolean washing() {
-        if (full < 0) {
+        if (satiety <= 0) {
             return false;
-        } else {
-            full -= 1;
-            System.out.println("Cat is washing");
-            System.out.println("Fullness: " + full + "\n");
-            return true;
         }
+        satiety -= 1;
+        System.out.println("Cat is washing");
+        System.out.println("Fullness: " + satiety + "\n");
+        return true;
     }
 }
+
